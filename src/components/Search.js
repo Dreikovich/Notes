b5f59abceda434185f9a88ab6fead045896e1192
drewkevich <AndrzejHK@gmail.com>
2022-05-22 23:10:46 +0200

    add search function

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
-import Button  from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {faSearch} from "@fortawesome/free-solid-svg-icons";
 import Badge from "react-bootstrap/Badge";
